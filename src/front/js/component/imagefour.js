5cec5b14db5b6e0bfbda76be5b8621ac6ae570e5
lucciii33 <angelomaiele@gmail.com>
2022-05-31 00:20:34 +0000

    grammar

--- src/front/js/component/imagefour.js
+++ src/front/js/component/imagefour.js
@@ -10,8 +10,8 @@ export const ImageFour = () => {
 			<div className="centerMars4">
 				<div className="center">
 
-					<h2>Did you know there is a robot in mars thaking pictoures?</h2>
-					<h4>its called Cutiosity</h4>
+					<h2>Did you know there is a robot on mars taking pictures?</h2>
+					<h4>it's called Curiosity</h4>
 					<p>The Mars rover Curiosity is not just one of the cutest robots you’ll ever see, but also an important part of NASA’s exploration of Mars. Since it touched down on the red planet on August 5, 2012, Curiosity has worked diligently – collecting rock samples & taking well over 300,000 images of Mars. </p>
 					<Link to="/marsview"><button type="button" class="btn btn-outline-light">see photos in mars</button></Link>
 
